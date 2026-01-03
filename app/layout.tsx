bf17a234af19037d36f8231998ab30b25fc61a16
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-01-03 09:17:13 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
